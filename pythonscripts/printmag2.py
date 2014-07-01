ce61caa31128ab953aa5963e129fbc087f7b2df9
blondegeek <tsmidt@berkeley.edu>
2014-07-01 12:51:52 -0700

    updated print magnetization files -- note these are assuming a particular unit cell so files will have to be tweaked

--- pythonscripts/printmag2.py
+++ pythonscripts/printmag2.py
@@ -32,11 +32,20 @@ magx3list = [np.absolute(x) for x in magxlist]
 magy3list = [np.absolute(x) for x in magylist]
 magz3list = [np.absolute(x) for x in magzlist]
 
+magnorm = [np.sqrt(magx2list[i]+magy2list[i]+magz2list[i]) for i in range(0,len(magx2list))]
+
+print "average mag moment on each ion"
+print str(np.sqrt(sum(magnorm)/len(magnorm)))
+
 print "quadrature"
 print str(np.sqrt(sum(magx2list)/len(magx2list)))
 print str(np.sqrt(sum(magy2list)/len(magy2list)))
 print str(np.sqrt(sum(magz2list)/len(magz2list)))
 
+print "mag average in each direction"
+print str(sum(magx3list)/len(magx3list))
+print str(sum(magy3list)/len(magy3list))
+print str(sum(magz3list)/len(magz3list))
 
 print "summed average"
 print str(sum(magxlist)/len(magxlist))
