ebadd729f9e4310d8ba204432decf3fbf0672949
blondegeek <tsmidt@berkeley.edu>
2014-12-03 16:33:11 -0800

    added to submit script so i don't submit jobs in same directory twice

--- bashscripts/submit.sh
+++ bashscripts/submit.sh
@@ -1,8 +1,15 @@
-file=`ls *sjob` 
-for each in $file
-do
-	echo "$file"
-	echo $(date) >> ~/joblog
-	sbatch $file >> ~/joblog
-	echo `pwd` >> ~/joblog
-done
+pathy=`pwd`
+
+if squeue | grep tsmidt | awk '{ print $1 }' | xargs -n 1 sh ~/bashscripts/jerbquery.sh | grep -q $pathy
+then
+   echo "Job already running in this directory"
+else
+   file=`ls *sjob` 
+   for each in $file
+   do
+   	echo "$file"
+   	echo $(date) >> ~/joblog
+   	sbatch $file >> ~/joblog
+   	echo `pwd` >> ~/joblog
+   done
+fi
