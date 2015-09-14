a730501e667065efdc3453d9fb674486b7663774
blondegeek <tsmidt@berkeley.edu>
2015-09-14 16:34:02 -0700

    added prettified getBondAngles script

--- pythonscripts/getBondsAngles.py
+++ pythonscripts/getBondsAngles.py
@@ -1,28 +1,3 @@
-'''
-Created on September 22, 2013
-
-@author: Tess
-
-This script is to help quickly assess basic information about symmetrized lithium 
-iridate structures. Note that if you give a relaxed structure from a calculation with
-ISYM=0 your CONTCAR may not have the same symmetry as your POSCAR did. See getsymwyck.py, refined.py, 
-conv.py, and primitive.py scripts to symmetrize your CONTCAR file such that you can
-compare it to multiple runs.
-
-This script prints out the:
-formula
-spacegroup
-lattice vector magnitudes a b c
-lattice vector angles
-number of sites
------
-wyckoff positions
------
-distinct Ir-Ir bonds
------
-distinct Ir-O-Ir angles
-
-'''
 
 import numpy as np
 import math
@@ -30,8 +5,6 @@ import pymatgen
 from pymatgen.symmetry.finder import SymmetryFinder
 
 s = pymatgen.read_structure('POSCAR2')
-
-# GET BASIC STRUCTURAL INFO
 
 # composition
 print "Formula:\t"+str(s.composition.formula)
@@ -102,46 +75,48 @@ for each in unique:
 
 print "---------"
 
-# print distinct Ir-Ir bonds
-print "Distinct Ir-Ir Bond Lengths"
 
+def getDistinctLengths(listOne,listTwo,length,tolerance,s,atomNumber,equiv):
+	equivcounter=-1
+	distances=list()
+	for i in listOne:
+		if equivcounter!=equiv[i]:
+			equivcounter=equiv[i]
+			neigh = s.get_neighbors_in_shell(s[i].coords,length,tolerance)
+			for each in neigh:
+				if each[2] in listTwo:
+					distances.append([sorted([atomNumber[i],atomNumber[each[2]]]),round(each[1],5)])
+	distinctDistances=list()
+	map(lambda x: not x in distinctDistances and distinctDistances.append(x), distances)
+	
+	prettyDistinctDistances = [ ["-".join(str(e) for e in each[0]),str(each[1])] for each in distinctDistances]
+	for each in prettyDistinctDistances:
+	        print "\t".join(each)
+	
+	print "---------"
 
-#Ir bonds are usually 3.0+/-0.3 AA away (for lithium and sodium iridate)
+getDistinctLengths(iridiums,oxygens,2.0,0.5,s,atomNumber,equiv)
+getDistinctLengths(lithiums,oxygens,2.0,0.5,s,atomNumber,equiv)
+getDistinctLengths(iridiums,iridiums,3.0,0.5,s,atomNumber,equiv)
+getDistinctLengths(lithiums,lithiums,3.0,0.5,s,atomNumber,equiv)
+getDistinctLengths(lithiums,iridiums,3.0,0.5,s,atomNumber,equiv)
 
-equivcounter=-1
-distances=list()
-for i in iridiums:
-	if equivcounter!=equiv[i]:
-		equivcounter=equiv[i]
-		neigh = s.get_neighbors_in_shell(s[i].coords,3.0,0.3)
-		for each in neigh:
-			if each[2] in iridiums:
-				distances.append([sorted([atomNumber[i],atomNumber[each[2]]]),round(each[1],5)])
-distinctDistances=list()
-map(lambda x: not x in distinctDistances and distinctDistances.append(x), distances)
-
-prettyDistinctDistances = [ ["-".join(str(e) for e in each[0]),str(each[1])] for each in distinctDistances]
-for each in prettyDistinctDistances:
-        print "\t".join(each)
-
-print "---------"
-
-
-# get distinct Ir-O-Ir angles
-
-
-print "Distinct Ir-O-Ir Bond Angles"
-
-counter=[0,0,0]
-for i in oxygens[0::1]:
-	thisONeigh=s.get_neighbors_in_shell(s[i].coords,2.1,0.3)
-	IrList=[site[2] for site in thisONeigh if site[2] in iridiums]
-	IrListCoord=[site[0] for site in thisONeigh if site[2] in iridiums]
-	threeEquiv = [atomNumber[IrList[0]],atomNumber[i],atomNumber[IrList[1]]]
-	if counter!=sorted(threeEquiv):
-		counter=sorted(threeEquiv)
+def getDistinctAngles(listOne,listTwo,length,tolerance,s,atomNumber,equiv):
+	counter=[0,0,0]
+	angleSet = []
+	for i in listTwo[0::1]:
+		thisONeigh=s.get_neighbors_in_shell(s[i].coords,length,tolerance)
+		IrList=[site[2] for site in thisONeigh if site[2] in listOne]
+		IrListCoord=[site[0] for site in thisONeigh if site[2] in listOne]
+		IrOpt = sorted([atomNumber[IrList[0]],atomNumber[IrList[1]]])
+		threeEquiv = [IrOpt[0],atomNumber[i],IrOpt[1]]
 		A=s[i].coords-IrListCoord[0].coords
 		B=s[i].coords-IrListCoord[1].coords
 		angle=str(round(np.degrees(math.acos(np.dot(A,B)/(np.linalg.norm(A)*np.linalg.norm(B)))),2))
 		angleRow=["-".join(str(each) for each in threeEquiv),angle]
-		print "\t".join(angleRow)	
+		angleSet.append("\t".join(angleRow))
+	for s in set(angleSet):
+		print s
+	print "---------"
+
+getDistinctAngles(iridiums,oxygens,2,0.8,s,atomNumber,equiv)
